f1c573ae39aee0ba59f672ca8a7d541a7e5cde28
Felipe Duarte <felipesilvapac@gmail.com>
2021-07-03 21:01:50 -0300

    passando metodo post

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,9 @@ app.get("/users", (request, response) => {
   return response.json({message : "Server Online"})
 });
 
+app.post("/", (request, response) => {
+  return response.json({message : "Os dados foram salvos"})
+});
 
 
 app.listen(3333, () => console.log("Server is running"));
