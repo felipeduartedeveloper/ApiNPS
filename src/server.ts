1d06eb0d9527f7562610bcdb1c9f03f7c2568dc7
Felipe Duarte <felipesilvapac@gmail.com>
2021-07-03 21:22:27 -0300

    implementando banco

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata'
 import express from 'express';
-
+import "./database";
 const app = express();
+
 
 app.get("/users", (request, response) => {
   return response.json({message : "Server Online"})
