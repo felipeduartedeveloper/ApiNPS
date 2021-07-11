b6ab9e10bc4991afe602b06fae95f3965f0bb8a7
Felipe Duarte <felipesilvapac@gmail.com>
2021-07-11 19:48:28 -0300

    resolve erro de inicialização

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,7 +4,9 @@ export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      database: process.env.NODE_ENV === 'test' ? "./src/database/database.test.sqlite" : defaultOptions.database,
+      database: process.env.NODE_ENV === 'test' 
+      ? "./src/database/database.test.sqlite" 
+      : defaultOptions.database
     })
   );
 };
